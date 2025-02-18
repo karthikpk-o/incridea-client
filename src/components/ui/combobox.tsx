12c54d4144ae8ee5e7366f0cfdd32b569c738fbb
Rahul <bn2345890@gmail.com>
2025-02-18 19:40:06 +0530

    fix: fix form moving left and also made combobox stay down

--- src/components/ui/combobox.tsx
+++ src/components/ui/combobox.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { Check, ChevronsUpDown } from "lucide-react";
 import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
 
 import { Button, type ButtonProps } from "~/components/ui/button";
 import {
   Command,
   CommandEmpty,
-  CommandList,
+  CommandGroup,
   CommandInput,
   CommandItem,
-  CommandGroup,
+  CommandList,
 } from "~/components/ui/command";
 import {
   Popover,
@@ -21,17 +21,13 @@ import {
 import { cn } from "~/lib/utils";
 
 type Props = ButtonProps & {
-  data: {
-    id: string;
-    name: string;
-    [key: string]: unknown;
-  }[];
+  data: { id: string; name: string; [key: string]: unknown }[];
   value?: string;
   setValue?: (value: string) => void;
   placeholder: string;
   keywords?: string[];
   popoverClassName?: string;
-}
+};
 
 const ComboBox: React.FunctionComponent<Props> = ({
   data,
@@ -49,70 +45,75 @@ const ComboBox: React.FunctionComponent<Props> = ({
   const containerRef = React.useRef<HTMLDivElement>(null);
 
   return (
-    <div ref={containerRef} >
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            {...props}
-            variant="outline"
-            role="combobox"
-            aria-expanded={open}
-            className={cn("justify-between truncate", className)}
-          >
-
-            {data.find((ele) => ele.id === value)?.name ?? placeholder}
-            <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent
-          container={containerRef.current}
-          className={cn("p-0", popoverClassName)}
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          {...props}
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className={cn("w-full justify-between truncate", className)}
         >
-          <Command
-            loop
-          >
-            <CommandInput placeholder={placeholder} />
-            <CommandList>
-              <CommandEmpty >No results. {children}</CommandEmpty>
-              <CommandGroup>
-                {data.map((ele) => (
-                  <CommandItem
-                    className="font-trap"
-                    key={ele.id}
-                    value={ele.id}
-                    onSelect={(currentValue) => {
-                      setValue?.(currentValue === value ? "" : ele.id);
-                      setOpen(false);
-                    }}
-                    // By default, command searches in the values provided
-                    // Since we want to uniquely identify each item we have passed the id
-                    // But we are not searching indexed on the id, rather indexed on the name
-                    // Hence we provide additional keywords to match name and get correct results
-                    // FIXME(Omkar): This still searches against id provided, implement custom filter() on Command
-                    keywords={[
-                      ele.name,
-                      ...(keywords?.map((key) => {
-                        const temp = ele[key];
-                        if (typeof temp === "string") return temp;
-                        else return "";
-                      }) ?? []),
-                    ]}
-                  >
-                    <Check
-                      className={cn(
-                        "mr-2 h-4 w-4",
-                        value === ele.id ? "opacity-100" : "opacity-0",
-                      )}
-                    />
-                    {ele.name}
-                  </CommandItem>
-                ))}
-              </CommandGroup>
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
-    </div>
+          {data.find((ele) => ele.id === value)?.name ?? placeholder}
+          <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent
+        className={cn(
+          "fixed top-0 w-full -translate-x-1/2 p-0",
+          popoverClassName,
+        )}
+      >
+        <Command loop>
+          <CommandInput placeholder={placeholder} />
+          <CommandList>
+            <CommandEmpty>No results. {children}</CommandEmpty>
+            <CommandGroup>
+              {data.map((ele) => (
+                <CommandItem
+                  className="font-trap"
+                  key={ele.id}
+                  value={ele.id}
+                  onSelect={(currentValue) => {
+                    setValue?.(currentValue === value ? "" : ele.id);
+                    setOpen(false);
+                  }}
+                  onMouseEnter={(e) => {
+                    e.currentTarget.style.backgroundColor = "#ac7420";
+                    e.currentTarget.style.color = "white";
+                  }}
+                  onMouseLeave={(e) => {
+                    e.currentTarget.style.backgroundColor = "transparent";
+                    e.currentTarget.style.color = "black";
+                  }}
+                  // By default, command searches in the values provided
+                  // Since we want to uniquely identify each item we have passed the id
+                  // But we are not searching indexed on the id, rather indexed on the name
+                  // Hence we provide additional keywords to match name and get correct results
+                  // FIXME(Omkar): This still searches against id provided, implement custom filter() on Command
+                  keywords={[
+                    ele.name,
+                    ...(keywords?.map((key) => {
+                      const temp = ele[key];
+                      if (typeof temp === "string") return temp;
+                      else return "";
+                    }) ?? []),
+                  ]}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === ele.id ? "opacity-100" : "opacity-0",
+                    )}
+                  />
+                  {ele.name}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
   );
 };
 
